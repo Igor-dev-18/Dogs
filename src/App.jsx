c5aeb1afde0698b6bf0942958a7eb60035856757
Igor Mesquita <igormesquita719@gmail.com>
2023-03-06 15:14:00 -0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -18,33 +18,37 @@ import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
-      <UserStorage>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="login" element={<Login />}>
-            <Route index element={<LoginForm />} />
-            <Route path="criar" element={<LoginCreate />} />
-            <Route path="perdeu" element={<LoginPasswordLost />} />
-            <Route path="resetar" element={<LoginPasswordReset />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-          <Route
-            path="conta/*"
-            element={
-              <ProtectedRoute>
-                <User />
-              </ProtectedRoute>
-            }
-          ></Route>
-          <Route path="foto/:id" element={<Photo />} />
-          <Route path="perfil/:user" element={<UserProfile />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Footer />
-      </UserStorage>
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="login" element={<Login />}>
+                <Route index element={<LoginForm />} />
+                <Route path="criar" element={<LoginCreate />} />
+                <Route path="perdeu" element={<LoginPasswordLost />} />
+                <Route path="resetar" element={<LoginPasswordReset />} />
+                <Route path="*" element={<NotFound />} />
+              </Route>
+              <Route
+                path="conta/*"
+                element={
+                  <ProtectedRoute>
+                    <User />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route path="foto/:id" element={<Photo />} />
+              <Route path="perfil/:user" element={<UserProfile />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from './Footer.module.css';
+import Dogs from '../../Assets/dogs-footer.svg';
 
 function Footer() {
   return (
-    <div>Footer</div>
+   <footer className={styles.footer}>
+    <img src={Dogs} alt="Dogs logo"/>
+    <p>Dogs. Alguns direitos reservados.</p>
+   </footer>
   )
 }
 
