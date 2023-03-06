4e80a13bf0a8ba72ebb7890ea31c721d52ed46a1
Igor Mesquita <igormesquita719@gmail.com>
2023-03-06 16:30:33 -0300

    Lazy and Suspense

--- src/pages/User/UserStats.jsx
+++ src/pages/User/UserStats.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from "react";
+import React, { lazy, Suspense, useEffect } from "react";
 import { STATS_GET } from "../../api";
 import Head from "../../components/Helper/Head";
 import Loading from "../../components/Helper/Loading";
 import Error from "../../components/Helper/Error";
 import useFetch from "../../Hooks/useFetch";
-import UserStatsGraphs from "./UserStatsGraphs";
+const UserStatsGraphs = lazy(() => import("./UserStatsGraphs"));
 
 function UserStats() {
   const { data, error, loading, request } = useFetch();
@@ -12,7 +12,7 @@ function UserStats() {
   useEffect(() => {
     async function getData() {
       const { url, options } = STATS_GET();
-      const {response,json} = await request(url, options);
+      const { response, json } = await request(url, options);
       console.log(response);
     }
     getData();
@@ -22,10 +22,10 @@ function UserStats() {
   if (error) return <Error error={error} />;
   if (data) {
     return (
-      <section>
+      <Suspense fallback={<div></div>}>
         <Head title="Estatísticas" />
         <UserStatsGraphs data={data} />
-      </section>
+      </Suspense>
     );
   } else return null;
 }
